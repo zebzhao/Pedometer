97b081b078212c072b21ad305c5360123d0c690d
zeb <zeb.zhao@gamecheetah.net>
2016-03-19 23:37:01 -0400

    added MetricCalculator functions

--- src/main/java/de/j4velin/pedometer/MetricCalculator.java
+++ src/main/java/de/j4velin/pedometer/MetricCalculator.java
@@ -1,4 +1,15 @@
 package de.j4velin.pedometer;
 
 public class MetricCalculator {
+    public static float calculateDistance() {
+        return 0;
+    }
+
+    public static float calculateCalories() {
+        return 1;
+    }
+
+    public static int calculateSteps() {
+        return PedometerManager.steps.get(PedometerManager.steps.size() - 1) - PedometerManager.steps.get(0);
+    }
 }
